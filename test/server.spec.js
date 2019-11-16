e4398c4ed8de29c12f79438b4305b69df42ecdf3
jinkim-tech <jhkim8268@gmail.com>
2019-11-15 17:20:44 -0800

    updated tests

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,14 +1,23 @@
+/* eslint-disable no-unused-expressions */
+'use strict';
+const server = require('../server/server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../server/server');
+const expect = chai.expect;
 
 chai.use(chaiHttp);
-const expect = chai.expect;
 
 server.listen(4444);
 
-describe('loopback server', function() {
+describe('loopback Workshop', function() {
   this.timeout(5000);
+  beforeEach((done) => {
+    done();
+  });
+
+  afterEach((done) => {
+    done();
+  });
 
   it('responds to /', (done) => {
     chai.request(server)
@@ -20,24 +29,23 @@ describe('loopback server', function() {
       });
   });
 
-  // it('responds to /api/players', (done) => {
-  //   chai.request(server)
-  //     .get('/api/players')
-  //     .end((err, res) => {
-  //       expect(err).not.exist;
-  //       expect(res).to.have.status(200);
-  //       done();
-  //     });
-  // });
+  it('responds to /api/players', (done) => {
+    chai.request(server)
+      .get('/api/players')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
 
-  // it('responds to /teams', (done) => {
-  //   chai.request(server)
-  //     .get('/api/teams')
-  //     .end((err, res) => {
-  //       expect(err).not.exist;
-  //       expect(res).to.have.status(200);
-  //       done();
-  //     });
-  // });
-
-})
+  it('responds to /api/teams', (done) => {
+    chai.request(server)
+      .get('/api/teams')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
